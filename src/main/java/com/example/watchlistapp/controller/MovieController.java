0693a164ff29ee9d91a3ae42da416aff51b4665f
DevSahil12 <sharmas80928@gmail.com>
2025-01-30 20:42:36 +0530

    change

--- src/main/java/com/example/watchlistapp/controller/MovieController.java
+++ src/main/java/com/example/watchlistapp/controller/MovieController.java
@@ -5,6 +5,7 @@ import com.example.watchlistapp.service.DatabaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
@@ -19,17 +20,27 @@ public class MovieController {
     DatabaseService service;
 
     @GetMapping(value = "/watchlistItemForm")
-    public ModelAndView showWatchListForm() {
+    public ModelAndView showWatchListForm(@RequestParam(required = false) Integer id) {
         //Fetch the form and Dynamically mapping with object
         String viewName = "watchlistItemForm";
         Map<String, Object> model = new HashMap<>();
-        model.put("watchlistItem", new Movie());//map with html 35
+        if (id != null) {
+            model.put("watchlistItem", service.getMovieById(id));
+        } else {
+            model.put("watchlistItem", new Movie());
+        }
         return new ModelAndView(viewName, model);
 
     }
 
     @PostMapping(value = "/watchlistItemForm")
     public ModelAndView submitWatchListForm(Movie movie) {
+        Integer id = movie.getId();
+        if (id == null) {
+            service.create(movie);
+        } else {
+            service.update(movie, id);
+        }
         service.create(movie);
         RedirectView rd = new RedirectView();
         rd.setUrl("/watchlist");
